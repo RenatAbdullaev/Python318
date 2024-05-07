31436b186a2172f5ef3abce1a292a49fa75dd9fe
renat <renabdullaev.1991@gmail.com>
2024-05-07 17:36:00 +0300

    hw10.py

--- dz/hw12.py
+++ dz/hw12.py
@@ -1,55 +1,164 @@
-class Car:
-    def __init__(self, model, year, manufacturer, engine_power, color, price):
-        self.model = model
-        self.year = year
-        self.manufacturer = manufacturer
-        self.engine_power = engine_power
-        self.color = color
-        self.price = price
+class Clock:
+    __DAY = 86400
 
-    def set_model(self, model):
-        self.model = model
+    def __init__(self, sec: int):
+        if not isinstance(sec, int):
+            raise ValueError("Секунды должны быть целым числом")
+        self.sec = sec % self.__DAY
 
-    def get_model(self):
-        return self.model
+    def get_format_time(self):
+        s = self.sec % 60
+        m = (self.sec // 60) % 60
+        h = (self.sec // 3600) % 24
+        return f"{Clock.__get_form(h)}:{Clock.__get_form(m)}:{Clock.__get_form(s)}"
 
-    def set_year(self, year):
-        self.year = year
+    @staticmethod
+    def __get_form(
+            x):  # не можем обратиться за пределами класса (два подчёркивания), он и не нужен (добавлять "0" к числу)
+        return str(x) if x > 9 else "0" + str(x)
 
-    def get_year(self):
-        return self.year
+    def __add__(self, other):  # для сложения -> перегрузка оператора для "+"
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом Clock")
+        return Clock(self.sec + other.sec)
 
-    def set_manufacturer(self, manufacturer):
-        self.manufacturer = manufacturer
+    def __sub__(self, other):  # для вычитания -> перегрузка оператора для "-"
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом Clock")
+        return Clock(self.sec - other.sec)
 
-    def get_manufacturer(self):
-        return self.manufacturer
+    def __mul__(self, other):  # для умножения -> перегрузка оператора для "*"
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом Clock")
+        return Clock(self.sec * other.sec)
 
-    def set_engine_power(self, engine_power):
-        self.engine_power = engine_power
+    # Проверка оператора деление -> действительно возникает ошибка, т.к. получаем тип данных float
+    # def __truediv__(self, other):  # для деления -> перегрузка оператора для "/"
+    #     if not isinstance(other, Clock):
+    #         raise ArithmeticError("Правый операнд должен быть типом Clock")
+    #     return Clock(self.sec / other.sec)  # ValueError: Секунды должны быть целым числом
 
-    def get_engine_power(self):
-        return self.engine_power
+    def __floordiv__(self, other):  # для целочисленного деления -> перегрузка оператора для "//"
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом Clock")
+        return Clock(self.sec // other.sec)
 
-    def set_color(self, color):
-        self.color = color
+    def __mod__(self, other):  # для остатка от деления -> перегрузка оператора для "%"
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом Clock")
+        return Clock(self.sec % other.sec)
 
-    def get_color(self):
-        return self.color
+    def __iadd__(self, other):  # для оператора +=
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом Clock")
+        self.sec += other.sec
+        return self
 
-    def set_price(self, price):
-        self.price = price
+    def __isub__(self, other):  # для оператора -=
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом Clock")
+        self.sec -= other.sec
+        return self
 
-    def get_price(self):
-        return self.price
+    def __imul__(self, other):  # для оператора *=
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом Clock")
+        self.sec *= other.sec
+        return self
 
-    def display_data(self):
-        print(" Данные автомобиля ".center(40, "*"))
-        print(f"Название модели: {self.model}\nГод выпуска: {self.year}\nПроизводитель: {self.manufacturer}\n"
-              f"Мощность двигателя: {self.engine_power}\nЦвет машины: {self.color}\nЦена: {self.price}")
-        print("Название модели:", self.model)
-        print("=" * 40)
+    def __ifloordiv__(self, other):  # для оператора /=
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом Clock")
+        self.sec //= other.sec
+        return self
+
+    def __imod__(self, other):
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом Clock")
+        self.sec %= other.sec
+        return self
+
+    # Сравнение
+    def __eq__(self, other):  # для оператора равенство -> перегрузка оператора для "=="
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом Clock")
+        return self.sec == other.sec
+
+    def __ne__(self, other):  # для оператора неравенство -> перегрузка оператора для "!="
+        return not self.__eq__(other)
+    # if not isinstance(other, Clock):  # для оператора неравенство (др. способ) -> перегрузка оператора для "!="
+    #     raise ArithmeticError("Правый операнд должен быть типом Clock")
+    # return self.sec != other.sec
+
+    def __lt__(self, other):
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом Clock")
+        return self.sec < other.sec
+
+    def __le__(self, other):
+        return self.__lt__(other) or self.__eq__(other)
+
+    def __gt__(self, other):
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом Clock")
+        return self.sec > other.sec
+
+    def __ge__(self, other):
+        return self.__gt__(other) or self.__eq__(other)
 
 
-car = Car("X7 M50i", 2021, "BMW", "530 л.с.", "white", 10790000)
-car.display_data()
+c1 = Clock(100)
+c2 = Clock(200)
+print(c1.get_format_time())  # 0:1:40; c __get_form(x) 00:01:40
+print(c2.get_format_time())  # 00:03:20
+c3 = c1 + c2  # только тип данных Clock
+print(c3.get_format_time())  # 00:05:00 Проверим работу "+=" - перезапись c1:
+# c1 += c2
+c4 = c1 + c2 + c3  # работает за счёт __add__
+print(c4.get_format_time())  # 00:10:00
+c5 = c4 - c2
+print(c5.get_format_time())  # 00:06:40
+c6 = c5 * c1
+print(c6.get_format_time())  # 11:06:40
+# c7 = c6 / c2
+# print(c7.get_format_time())  # ValueError: Секунды должны быть целым числом -> верно для типа данных float
+# Целочисленное деление
+c7 = (c6 // c5)  # 00:01:40
+print(c7.get_format_time())
+# Остаток от деления
+c8 = c6 % c5
+print(c8.get_format_time())  # 00:00:00
+# Операторы присваивания:
+c7 += c8
+print(c7.get_format_time())  # 00:01:40
+c6 -= c5
+print(c6.get_format_time())  # 11:00:00
+c4 *= c5
+print(c4.get_format_time())  # 18:40:00
+c6 //= c4
+print(c6.get_format_time())  # 00:00:00
+c4 %= c5
+print(c4.get_format_time())  # 00:00:00
+
+print("*" * 50)
+
+# Равенство
+if c1 == c2:
+    print("Время равно")
+else:
+    print("Время разное")
+# Неравенство
+if c1 != c2:
+    print("Время разное")
+else:
+    print("Время равно")
+# Сравнение
+if c1 < c2:
+    print("Время меньше")
+elif c1 <= c2:
+    print("Время меньше или равно")
+
+if c1 > c2:
+    print("Время больше")
+elif c1 >= c2:
+    print("Время больше или равно")
