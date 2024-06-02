cdd603edbe4200815b8943e75124aa4e78efafe4
renat <renabdullaev.1991@gmail.com>
2024-06-02 15:13:28 +0300

    hw10.py

--- dz/hw18.py
+++ dz/hw18.py
@@ -1,90 +1,35 @@
-import json
-
-data = {}
-
-
-class CountryCapital:
-    def __init__(self, country, capital):
-        self.country = country
-        self.capital = capital
-        data[self.country] = self.capital
-
-    def __str__(self):
-        return f"{self.country}: {self.capital}"
-
-    @staticmethod
-    def load_data(filename):
-        try:
-            data = json.load(open(filename))
-        except FileNotFoundError:
-            data = {}
-        finally:
-            return data
-
-    @staticmethod
-    def add_country(filename):
-        country_name = input("Введите название страны: ")
-        capital_name = input("Введите название столицы: ")
-        data = CountryCapital.load_data(filename)
-        data[country_name] = capital_name
-        with open(filename, "w") as f:
-            json.dump(data, f, indent=2)
-
-    @staticmethod
-    def load_from_file(filename):
-        with open(filename, "r") as f:
-            print(json.load(f))
-
-    @staticmethod
-    def delete_country(filename):
-        del_country = input("Введите название страны: ")
-        data = CountryCapital.load_data(filename)
-        if del_country in data:
-            del data[del_country]
-            with open(filename, "w") as f:
-                json.dump(data, f, indent=2)
-        else:
-            print("Такой страны в базе нет")
-
-    @staticmethod
-    def search_data(filename):
-        country = input("Введите название страны: ")
-        data = CountryCapital.load_data(filename)
-        if country in data:
-            print(f"Страна {country} столица {data[country]} есть в словаре")
-        else:
-            print(f"Страны {country} нет в словаре")
-
-    @staticmethod
-    def edit_data(filename):
-        country = input("Введите страну для корректировки: ")
-        new_capital = input("Введите новое название столицы: ")
-        data = CountryCapital.load_data(filename)
-        if country in data:
-            data[country] = new_capital
-            with open(filename, "w") as f:
-                json.dump(data, f, indent=2)
-        else:
-            print("Такой страны в базе нет")
+import requests
+from bs4 import BeautifulSoup
+import csv
 
 
-file = 'list_capital.json'
-index = ''
-while True:
-    index = input("Выбор действия:\n1 - добавление данных\n2 - удаление данных\n3 - поиск данных"
-                  "\n4 - редактирование данных\n5 - просмотр данных\n6 - завершение работы\nВвод: ")
-    if index == "1":
-        CountryCapital.add_country(file)
-    elif index == "2":
-        CountryCapital.delete_country(file)
-    elif index == "3":
-        CountryCapital.search_data(file)
-    elif index == "4":
-        CountryCapital.edit_data(file)
-    elif index == "5":
-        CountryCapital.load_from_file(file)
-    elif index == "6":
-        break
-    else:
-        print("Введен некорректный номер")
-        
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+
+def write_csv(data):
+    with open("links.csv", "a", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
+
+
+def get_data(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    ul = soup.find("ul", class_="navbar-nav")
+    links = ul.find_all("li")
+
+    for link in links:
+        url = link.find("a").get("href")
+        data = [url]
+        write_csv(data)
+
+
+def main():
+    url = "https://riptutorial.com/html/example/810/hello-world"
+    html = get_html(url)
+    get_data(html)
+
+
+if __name__ == '__main__':
+    main()
